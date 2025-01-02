c58b25f2d0e3aa8e5c6ba2c48a6603be9754ac82
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-02 10:57:01 +0530

    c4

--- calc.js
+++ calc.js
@@ -28,3 +28,11 @@ switch (operation) {
   case "sub":
     console.log(`🔢 ${num1} - ${num2} = ${num1 - num2}`);
     break;
+    case "mul":
+    console.log(`🔢 ${num1} * ${num2} = ${num1 * num2}`);
+    break;
+
+  case "div":
+    if (num2 === 0) {
+      console.log("🚫 Division by zero is not allowed.");
+    } 
