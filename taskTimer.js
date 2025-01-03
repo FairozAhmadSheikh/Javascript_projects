3c913dab7cfdafc57cbfb831cebf58312e68a940
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-03 14:15:26 +0530

    c2

--- taskTimer.js
+++ taskTimer.js
@@ -1,2 +1,9 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+function startTimer(minutes) {
+  let secondsLeft = minutes * 60;
+
+  const interval = setInterval(() => {
+    const min = Math.floor(secondsLeft / 60);
+    const sec = secondsLeft % 60;
+    console.log(`⏳ ${min}m ${sec}s`);
