eae1e77ee27df31fcc5bff32b572559546801aef
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-04 14:18:17 +0530

    c2

--- taskTimeradvance.js
+++ taskTimeradvance.js
@@ -1,2 +1,4 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+function startTimer(minutes) {
+  let secondsLeft = minutes * 60;
