bb141818ef4ad9000ba004579d9b8edcbe144b57
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-06 14:11:37 +0530

    c2

--- guess.js
+++ guess.js
@@ -1,2 +1,4 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const secret = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
