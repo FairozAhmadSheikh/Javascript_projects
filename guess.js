7557374d9d3d0a46d9f20d35afd789ce8b2ef303
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-06 14:11:42 +0530

    c3

--- guess.js
+++ guess.js
@@ -2,3 +2,6 @@ const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const secret = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
+function guess() {
+  rl.question("Guess a number (1-100): ", (num) => {
+    attempts++;
