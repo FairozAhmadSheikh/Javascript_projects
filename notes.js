e03656af49079ef4a8b0f72c210d514af3906c92
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-07 14:10:16 +0530

    c3

--- notes.js
+++ notes.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+rl.question("Enter your note: ", (note) => {
+  fs.appendFileSync('notes.txt', note + '\n');
