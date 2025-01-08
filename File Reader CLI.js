d443340f19bc29f0f36d20f689edea5c0e241e46
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-08 14:06:29 +0530

    c7

--- File Reader CLI.js
+++ File Reader CLI.js
@@ -5,3 +5,4 @@ rl.question("Enter file path to read: ", (path) => {
     fs.readFile(path, 'utf8', (err, data) => {
         if (err) return console.error("Error:", err.message);
         console.log("File Content:\n", data);
+        rl.close();
