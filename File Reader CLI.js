3503b18fab996ce9f6f47d5e4f6867a222fb7a22
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-08 14:06:03 +0530

    c4

--- File Reader CLI.js
+++ File Reader CLI.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+rl.question("Enter file path to read: ", (path) => {
