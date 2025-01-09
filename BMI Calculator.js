88b624ce5789cf9f471a420bd6fade9518ad4a25
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-09 14:02:44 +0530

    c6

--- BMI Calculator.js
+++ BMI Calculator.js
@@ -4,3 +4,5 @@ rl.question("Enter your weight (kg): ", (weight) => {
     rl.question("Enter your height (m): ", (height) => {
         const bmi = parseFloat(weight) / (parseFloat(height) ** 2);
         console.log(`Your BMI is: ${bmi.toFixed(2)}`);
+        if (bmi < 18.5) console.log("Underweight");
+        else if (bmi < 24.9) console.log("Normal");
