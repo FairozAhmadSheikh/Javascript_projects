5d2143ba11cb9385a4e9b553a205a9db1d46f407
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-09 14:02:06 +0530

    c3

--- BMI Calculator.js
+++ BMI Calculator.js
@@ -1,3 +1,4 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 rl.question("Enter your weight (kg): ", (weight) => {
+    rl.question("Enter your height (m): ", (height) => {
