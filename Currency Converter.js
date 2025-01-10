828336784db15b7306f6d7fa68615b56d63d8a47
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-10 13:59:38 +0530

    c6

--- Currency Converter.js
+++ Currency Converter.js
@@ -4,3 +4,4 @@ const rates = { USD: 83, EUR: 90, GBP: 105 };
 rl.question("Enter amount in INR: ", (amount) => {
      console.log(`USD: ${(amount / rates.USD).toFixed(2)}`);
      console.log(`EUR: ${(amount / rates.EUR).toFixed(2)}`);
+     console.log(`GBP: ${(amount / rates.GBP).toFixed(2)}`);
