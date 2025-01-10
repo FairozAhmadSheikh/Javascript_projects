97173c06f09aac876843b74d4bcb7a37467590d2
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-10 13:59:56 +0530

    c7

--- Currency Converter.js
+++ Currency Converter.js
@@ -2,7 +2,7 @@ const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const rates = { USD: 83, EUR: 90, GBP: 105 };
 rl.question("Enter amount in INR: ", (amount) => {
-     console.log(`USD: ${(amount / rates.USD).toFixed(2)}`);
-     console.log(`EUR: ${(amount / rates.EUR).toFixed(2)}`);
-     console.log(`GBP: ${(amount / rates.GBP).toFixed(2)}`);
+    console.log(`USD: ${(amount / rates.USD).toFixed(2)}`);
+    console.log(`EUR: ${(amount / rates.EUR).toFixed(2)}`);
+    console.log(`GBP: ${(amount / rates.GBP).toFixed(2)}`);
     rl.close();
