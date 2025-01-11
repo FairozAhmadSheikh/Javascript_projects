866bc506dd3a4a7400d8c5a31e80f67b57fe2b3d
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-11 13:56:35 +0530

    c2

--- Random Password Generator.js
+++ Random Password Generator.js
@@ -1,2 +1,3 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
