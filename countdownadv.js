3480e81e8c098a289453a2b2ab396f9045dedb79
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-12 13:54:26 +0530

    c3

--- countdownadv.js
+++ countdownadv.js
@@ -1,2 +1,5 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+rl.question("Enter countdown time in seconds: ", (sec) => {
+  let time = parseInt(sec);
