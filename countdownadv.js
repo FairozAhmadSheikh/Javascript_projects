ee6c8de4b090e1a8019a43513231183060710345
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-12 13:54:52 +0530

    c4

--- countdownadv.js
+++ countdownadv.js
@@ -4,4 +4,6 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
 rl.question("Enter countdown time in seconds: ", (sec) => {
   let time = parseInt(sec);
    const interval = setInterval(() => {
-    if (time <= 0)
+    if (time <= 0){
+      console.log("⏰ Time's up!");
+      clearInterval(interval);
