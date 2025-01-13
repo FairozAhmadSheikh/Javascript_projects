594410b89638d5db5913f55e9a438d2b112d55ea
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-13 13:50:13 +0530

    c2

--- adv_Quiz App.js
+++ adv_Quiz App.js
@@ -1,2 +1,5 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const questions = [
+  { q: "Capital of France?", a: "paris" },
+  { q: "5 + 3 = ?", a: "8" },
