0f69ac11d6f140026ee5801b08246e3958542308
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-14 13:48:49 +0530

    c6

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -4,4 +4,4 @@ rl.question("Enter a string to check for palindrome: ", (str) => {
   const cleaned = str.replace(/[\W_]/g, '').toLowerCase();
   const reversed = cleaned.split('').reverse().join('');
   console.log(cleaned === reversed ? "✅ It's a palindrome!" : "❌ Not a palindrome.");
-  
+  rl.close();
