fea8d26b4589e1281e9f6c1cd36d5061077cf984
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-16 13:36:30 +0530

    c4

--- todo_listmanager.js
+++ todo_listmanager.js
@@ -4,3 +4,11 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 let todos = [];
+function showMenu() {
+  console.log("\nTodo CLI");
+  console.log("1. Add Todo");
+  console.log("2. List Todos");
+  console.log("3. Delete Todo");
+  console.log("4. Exit");
+  rl.question("Choose an option: ", handleInput);
+}
