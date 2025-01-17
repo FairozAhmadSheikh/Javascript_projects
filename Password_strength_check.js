f3f7e944a275f6d72e649c6b766a987ccb25cd0d
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-17 13:32:12 +0530

    c4

--- Password_strength_check.js
+++ Password_strength_check.js
@@ -8,3 +8,9 @@ rl.question("Enter your password: ", (password) => {
   const length = password.length >= 8;
   const upper = /[A-Z]/.test(password);
   const lower = /[a-z]/.test(password);
+  const digit = /\d/.test(password);
+  const special = /[\W]/.test(password);
+
+  const passed = [length, upper, lower, digit, special].filter(Boolean).length;
+
+  if (passed === 5)
