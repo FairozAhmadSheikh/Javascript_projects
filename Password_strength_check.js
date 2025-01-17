2487a217115e9664dc1791567c762b25e2601525
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-17 13:32:01 +0530

    c3

--- Password_strength_check.js
+++ Password_strength_check.js
@@ -4,3 +4,7 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+rl.question("Enter your password: ", (password) => {
+  const length = password.length >= 8;
+  const upper = /[A-Z]/.test(password);
+  const lower = /[a-z]/.test(password);
