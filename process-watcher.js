b7172c1d4e9b8f0675fa57849ddab5e9d0bb8e57
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-19 16:04:06 +0530

    c3

--- process-watcher.js
+++ process-watcher.js
@@ -2,3 +2,5 @@ const { exec } = require("child_process")
 const os = require("os")
 const args = process.argv.slice(2)
 const filter = args[0]?.toLowerCase() || ""
+const isWindows = os.platform() === "win32"
+const command = isWindows ? "tasklist" : "ps -A"
