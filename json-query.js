58f18eaa39f9dfc8c6575ab4c56fc9bd6ed9125b
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-20 16:01:37 +0530

    c2

--- json-query.js
+++ json-query.js
@@ -3,3 +3,7 @@ const fs = require("fs")
 const args = process.argv.slice(2)
 const filePath = args[0]
 const query = args[1]
+if (!filePath || !query) {
+  console.log("Usage: json-query.js <file.json> <dot.notation.query>")
+  process.exit(1)
+}
