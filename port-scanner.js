31ad86079edaca3eed1d6fc083190acd9a104c8a
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-21 15:57:09 +0530

    c3

--- port-scanner.js
+++ port-scanner.js
@@ -4,3 +4,7 @@ const args = process.argv.slice(2)
 const host = args[0]
 const startPort = parseInt(args[1]) || 1
 const endPort = parseInt(args[2]) || 1024
+if (!host) {
+  console.log("Usage: port-scanner.js <host> [startPort] [endPort]")
+  process.exit(1)
+}
