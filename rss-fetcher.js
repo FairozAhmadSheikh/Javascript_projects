f33cf4ec031f571269adced042f6ee722194559f
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-22 15:54:54 +0530

    c3

--- rss-fetcher.js
+++ rss-fetcher.js
@@ -2,3 +2,7 @@ const https = require("https")
 const { parse } = require("node-html-parser")
 const args = process.argv.slice(2)
 const feedUrl = args[0]
+if (!feedUrl || !feedUrl.startsWith("http")) {
+  console.log("Usage: rss-fetcher.js <rss-feed-url>")
+  process.exit(1)
+}
