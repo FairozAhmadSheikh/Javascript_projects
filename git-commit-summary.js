4dbf18bafcb6fe1da390c00fc935c90992fcfb7d
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-23 15:51:06 +0530

    c3

--- git-commit-summary.js
+++ git-commit-summary.js
@@ -2,3 +2,6 @@ const { execSync } = require("child_process")
 try {
   const log = execSync("git log --pretty=format:'%an|%s'", { encoding: "utf8" })
   const lines = log.split("\n")
+  const summary = {}
+
+  lines.forEach(line => {
