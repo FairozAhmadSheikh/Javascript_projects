c33cb9ee7a565fcd2a6ffac945f6e9ba52f5e0db
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-24 15:46:08 +0530

    c3

--- log-analyzer.js
+++ log-analyzer.js
@@ -3,3 +3,7 @@ const readline = require("readline")
 const path = require("path")
 const args = process.argv.slice(2)
 const filePath = args[0]
+if (!filePath || !fs.existsSync(filePath)) {
+  console.log("Usage: log-analyzer.js <path-to-log-file>")
+  process.exit(1)
+}
