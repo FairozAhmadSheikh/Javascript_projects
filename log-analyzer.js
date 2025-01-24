004c6d2137c77b3041f6015b9164f066f5764947
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-24 15:46:54 +0530

    c5

--- log-analyzer.js
+++ log-analyzer.js
@@ -14,3 +14,13 @@ const rl = readline.createInterface({
   input: fs.createReadStream(filePath),
   crlfDelay: Infinity
 })
+rl.on("line", line => {
+  totalLines++
+
+  const parts = line.split(" ")
+  const ip = parts[0]
+  const status = parts[8]
+
+  if (ip) ipCount[ip] = (ipCount[ip] || 0) + 1
+  if (status) statusCount[status] = (statusCount[status] || 0) + 1
+})
