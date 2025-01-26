5c07cfd04dedfddd61e6191060f3c0ded69b0e5d
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-26 15:17:45 +0530

    c2

--- temp-covnvert.js
+++ temp-covnvert.js
@@ -1,3 +1,7 @@
 const args = process.argv.slice(2)
 const value = parseFloat(args[0])
 const unit = args[1]?.toLowerCase()
+if (isNaN(value) || !unit) {
+  console.log("Usage: temp-convert.js <value> <c|f|k>")
+  process.exit(1)
+}
