94da6f398b29898d9e3d1490bcd9f8a35146d87f
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-27 15:15:55 +0530

    done

--- random_quote.js
+++ random_quote.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
+
 const quotes = [
   "Stay hungry stay foolish",
   "The only way to do great work is to love what you do",
   "Life is what happens when you're busy making other plans",
   "You miss 100 percent of the shots you don’t take",
-    "In the middle of difficulty lies opportunity"
-    ]
+  "In the middle of difficulty lies opportunity"
+]
 
 const index = Math.floor(Math.random() * quotes.length)
 console.log(quotes[index])
