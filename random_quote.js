0899aecd9455f6c8d139ece6170c990b7e522e8e
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-27 15:15:22 +0530

    c3

--- random_quote.js
+++ random_quote.js
@@ -2,3 +2,4 @@ const quotes = [
   "Stay hungry stay foolish",
   "The only way to do great work is to love what you do",
   "Life is what happens when you're busy making other plans",
+  "You miss 100 percent of the shots you don’t take",
