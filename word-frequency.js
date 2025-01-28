c79f99bd18402f89b1df551beb4a02c21fd3f4b5
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-28 15:12:16 +0530

    c2

--- word-frequency.js
+++ word-frequency.js
@@ -2,3 +2,7 @@ const fs = require("fs")
 
 const args = process.argv.slice(2)
 const filePath = args[0]
+if (!filePath) {
+  console.log("Usage: word-frequency.js <file-path>")
+  process.exit(1)
+}
