a29e866b4fb25ab3af610893eb9e35b14160d78b
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-29 13:23:22 +0530

    c3

--- files_ize.js
+++ files_ize.js
@@ -2,3 +2,7 @@ const fs = require("fs")
 const path = require("path")
 const args = process.argv.slice(2)
 const filePath = args[0]
+if (!filePath) {
+  console.log("Usage: file-size.js <path-to-file>")
+  process.exit(1)
+}
