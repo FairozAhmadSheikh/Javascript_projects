ce7e96baa50cd0b1666884cbadd00af19f242084
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-31 13:20:26 +0530

    c7

--- uptime.js
+++ uptime.js
@@ -1,6 +1,11 @@
+#!/usr/bin/env node
+
 const os = require("os")
+
 const uptimeSeconds = os.uptime()
+
 const hours = Math.floor(uptimeSeconds / 3600)
 const minutes = Math.floor((uptimeSeconds % 3600) / 60)
 const seconds = uptimeSeconds % 60
+
 console.log(`System Uptime: ${hours}h ${minutes}m ${seconds}s`)
