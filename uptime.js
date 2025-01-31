c4d2bcd22b80379f10736cc9c629b87453df1a43
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-01-31 13:20:11 +0530

    c5

--- uptime.js
+++ uptime.js
@@ -2,3 +2,4 @@ const os = require("os")
 const uptimeSeconds = os.uptime()
 const hours = Math.floor(uptimeSeconds / 3600)
 const minutes = Math.floor((uptimeSeconds % 3600) / 60)
+const seconds = uptimeSeconds % 60
