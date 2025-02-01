069f13fc667051254f6ee027603ab649a0a7bf4d
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-01 13:15:45 +0530

    c2

--- disk-usage.js
+++ disk-usage.js
@@ -2,3 +2,8 @@ const { exec } = require("child_process")
 
 exec("df -h", (err, stdout, stderr) => {
   if (err || stderr) {
+    console.error("Error retrieving disk usage")
+    process.exit(1)
+  }
+  console.log(stdout)
+})
