04af5b08a241a413d3c75be9549ca466ff0eb783
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-01 13:16:37 +0530

    c7

--- memory_usage.js
+++ memory_usage.js
@@ -5,3 +5,4 @@ const used = total - free
 const percent = (used / total) * 100
 console.log(`Total Memory: ${(total / 1024 / 1024).toFixed(2)} MB`)
 console.log(`Used Memory: ${(used / 1024 / 1024).toFixed(2)} MB`)
+console.log(`Free Memory: ${(free / 1024 / 1024).toFixed(2)} MB`)
