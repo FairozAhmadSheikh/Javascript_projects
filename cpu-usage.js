850e82053edcbc5b4482a651a81e950160ac9c1e
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-02 13:13:49 +0530

    c2

--- cpu-usage.js
+++ cpu-usage.js
@@ -4,3 +4,9 @@ function getCpuUsage() {
   const cpus = os.cpus()
   let totalIdle = 0
   let totalTick = 0
+  cpus.forEach(cpu => {
+    for (let type in cpu.times) {
+      totalTick += cpu.times[type]
+    }
+    totalIdle += cpu.times.idle
+  })
