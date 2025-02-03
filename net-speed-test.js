8e9e846fa4368e24826f55c213e594dcb426aaa9
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-03 13:11:11 +0530

    c3

--- net-speed-test.js
+++ net-speed-test.js
@@ -3,3 +3,11 @@ const { performance } = require("perf_hooks")
 const url = "https://speed.hetzner.de/100MB.bin"
 
 console.log("Testing download speed...")
+const start = performance.now()
+
+https.get(url, res => {
+  let downloaded = 0
+
+  res.on("data", chunk => {
+    downloaded += chunk.length
+  })
