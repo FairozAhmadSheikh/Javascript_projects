f8f8b9d111311c27abf09de36dd2d3f2fb175146
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-05 13:07:32 +0530

    c3

--- portCheck.js
+++ portCheck.js
@@ -3,3 +3,7 @@ const net = require("net")
 const args = process.argv.slice(2)
 const port = parseInt(args[0], 10) || 3000
 const server = net.createServer()
+
+server.once("error", err => {
+  if (err.code === "EADDRINUSE") {
+    console.log(`Port ${port} is in use`)
