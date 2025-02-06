2bdf946e4b0a8ae8ac716eb396fe4bee6298562f
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-06 13:04:59 +0530

    c2

--- devlog-cli.js
+++ devlog-cli.js
@@ -1,3 +1,6 @@
 const fs = require("fs")
 const path = require("path")
 const readline = require("readline")
+const file = path.join(__dirname, "devlog.txt")
+const args = process.argv.slice(2)
+const command = args[0]
