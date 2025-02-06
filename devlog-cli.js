d6efce3be48a8cd65b7c66d48cd24b2462fe0688
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-06 13:05:05 +0530

    c3

--- devlog-cli.js
+++ devlog-cli.js
@@ -4,3 +4,7 @@ const readline = require("readline")
 const file = path.join(__dirname, "devlog.txt")
 const args = process.argv.slice(2)
 const command = args[0]
+if (!command) {
+  console.log("Usage: devlog-cli.js [add | view | clear]")
+  process.exit(1)
+}
