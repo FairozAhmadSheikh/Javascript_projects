ebcdacb1905b09391087ece88ee6619f053dbad1
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-10 12:54:38 +0530

    c3

--- todo-cli.js
+++ todo-cli.js
@@ -4,3 +4,10 @@ const path = require("path")
 const file = path.join(__dirname, "todo.json")
 const args = process.argv.slice(2)
 const command = args[0]
+
+
+function loadTodos() {
+  if (!fs.existsSync(file)) return []
+  const data = fs.readFileSync(file, "utf8")
+  return JSON.parse(data)
+}
