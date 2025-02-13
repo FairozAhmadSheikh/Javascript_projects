5a10f07b0d4bd58e149fbc27f605afb97430b584
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-13 12:49:21 +0530

    c7

--- sort-lines.js
+++ sort-lines.js
@@ -1,13 +1,21 @@
+#!/usr/bin/env node
+
 const fs = require("fs")
+
 const args = process.argv.slice(2)
 const filePath = args[0]
+
 if (!filePath) {
   console.log("Please provide a file path")
   process.exit(1)
 }
+
 fs.readFile(filePath, "utf8", (err, data) => {
   if (err) {
     console.error("Error reading file:", err.message)
     process.exit(1)
   }
+
   const sorted = data.split("\n").sort().join("\n")
+  console.log(sorted)
+})
