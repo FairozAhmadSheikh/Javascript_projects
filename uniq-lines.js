dd2d4b5ed2e3dcfb4f7f2bae544eae5be7fa62be
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-14 12:47:33 +0530

    c7

--- uniq-lines.js
+++ uniq-lines.js
@@ -1,17 +1,22 @@
+#!/usr/bin/env node
+
 const fs = require("fs")
 
 const args = process.argv.slice(2)
 const filePath = args[0]
+
 if (!filePath) {
   console.log("Please provide a file path")
   process.exit(1)
 }
+
 fs.readFile(filePath, "utf8", (err, data) => {
   if (err) {
     console.error("Error reading file:", err.message)
     process.exit(1)
   }
-const lines = data.split("\n")
-const unique = [...new Set(lines)]
-console.log(unique.join("\n"))
+
+  const lines = data.split("\n")
+  const unique = [...new Set(lines)]
+  console.log(unique.join("\n"))
 })
