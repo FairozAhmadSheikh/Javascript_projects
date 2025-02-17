49a35c6f531a1cd1b16d3b6f8c15606b2d0fa021
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-17 12:06:53 +0530

    c2

--- calculator.js
+++ calculator.js
@@ -4,3 +4,6 @@ if (args.length !== 3) {
   console.log("Usage: node calc.js num1 operator num2")
   process.exit(1)
 }
+const num1 = parseFloat(args[0])
+const operator = args[1]
+const num2 = parseFloat(args[2])
