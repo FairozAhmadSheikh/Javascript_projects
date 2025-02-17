ebd0d888687dd9d1c0ea9892e7f5b86779e32971
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-17 12:07:23 +0530

    c4

--- calculator.js
+++ calculator.js
@@ -16,4 +16,15 @@ let result
 switch (operator) {
   case "+":
     result = num1 + num2
+    break;
+    case "-":
+    result = num1 - num2
     break
+  case "*":
+    result = num1 * num2
+    break
+  case "/":
+    if (num2 === 0) {
+      console.log("Cannot divide by zero")
+      process.exit(1)
+    }
