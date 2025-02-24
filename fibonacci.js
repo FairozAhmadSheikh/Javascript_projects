fb6e1ee3dfcde6209c6ee5fa13a2049fa1a5f8be
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-24 11:55:23 +0530

    c2

--- fibonacci.js
+++ fibonacci.js
@@ -9,3 +9,18 @@ if (args.length !== 1) {
 `);
   process.exit(1);
 }
+const n = parseInt(args[0]);
+
+if (isNaN(n) || n < 1) {
+  console.log("❌ Please enter a positive integer greater than 0.");
+  process.exit(1);
+}
+
+function generateFibonacci(n) {
+  const fib = [0];
+  if (n > 1) fib.push(1);
+  for (let i = 2; i < n; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib;
+}
