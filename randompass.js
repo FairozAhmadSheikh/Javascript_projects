f9f321516e37f0bc75039bf29534602c035a2c0d
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-26 11:57:40 +0530

    c3

--- randompass.js
+++ randompass.js
@@ -1,3 +1,4 @@
 const args = process.argv.slice(2)
 const length = parseInt(args[0]) || 8
 const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+let password = ""
