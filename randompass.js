c9419b0c5977d672e26d9911f267c47457deb833
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-26 11:57:58 +0530

    c5

--- randompass.js
+++ randompass.js
@@ -4,3 +4,4 @@ const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#
 let password = ""
 for (let i = 0; i < length; i++) {
   const randomIndex = Math.floor(Math.random() * chars.length)
+  password += chars[randomIndex]
