33d7c6df4674dfe7d264d4b1fdfc494dc409b151
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-26 11:56:29 +0530

    c2

--- listdir.js
+++ listdir.js
@@ -3,3 +3,8 @@ const path = require("path")
 
 const args = process.argv.slice(2)
 const dirPath = args[0] || "."
+fs.readdir(dirPath, (err, files) => {
+  if (err) {
+    console.error("Error reading directory:", err.message)
+    process.exit(1)
+  }
