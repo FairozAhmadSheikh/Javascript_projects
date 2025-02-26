1e237be7477292052cb65eaed5b1fa058addd8d4
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-26 11:57:51 +0530

    c4

--- randompass.js
+++ randompass.js
@@ -2,3 +2,5 @@ const args = process.argv.slice(2)
 const length = parseInt(args[0]) || 8
 const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
 let password = ""
+for (let i = 0; i < length; i++) {
+  const randomIndex = Math.floor(Math.random() * chars.length)
