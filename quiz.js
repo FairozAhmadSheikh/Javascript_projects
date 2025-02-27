1c3839db9135f01106ca03336b7a831556bef997
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-02-27 11:20:13 +0530

    c2

--- quiz.js
+++ quiz.js
@@ -4,3 +4,14 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+const questions = [
+  {
+    question: "What is the capital of France?",
+    options: {
+      a: "Berlin",
+      b: "Paris",
+      c: "Madrid",
+      d: "Rome",
+    },
+    answer: "b",
+  },
