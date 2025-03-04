80fab37983f1253ed8635e31f0d26806150c42a0
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-03-04 15:03:31 +0530

    c2

--- http-status-checker.js
+++ http-status-checker.js
@@ -1,3 +1,10 @@
 const https = require("https")
 const http = require("http")
 const url = require("url")
+const args = process.argv.slice(2)
+const target = args[0]
+
+if (!target) {
+  console.log("Usage: http-status-checker.js <url>")
+  process.exit(1)
+}
