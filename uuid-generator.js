252c5217cc2518f5341cd0ef4b851190a63ba63c
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-03-06 15:05:22 +0530

    c2

--- uuid-generator.js
+++ uuid-generator.js
@@ -3,3 +3,10 @@ function generateUUID() {
   for (let i = 0; i < 36; i++) {
     if (i === 8 || i === 13 || i === 18 || i === 23) {
       uuid += "-"
+      } else if (i === 14) {
+      uuid += "4"
+    } else if (i === 19) {
+      uuid += (Math.floor(Math.random() * 4) + 8).toString(16)
+    } else {
+      uuid += Math.floor(Math.random() * 16).toString(16)
+    }
