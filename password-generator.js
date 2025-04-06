ed9dde30a5e1501bc5d379e5d41846845f827d6f
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-04-06 15:06:44 +0530

    c2

--- password-generator.js
+++ password-generator.js
@@ -2,3 +2,7 @@ const length = parseInt(process.argv[2]) || 12
 
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
 let password = ""
+for (let i = 0; i < length; i++) {
+  const index = Math.floor(Math.random() * charset.length)
+  password += charset[index]
+}
