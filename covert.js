87c408e4b93e532cda29990e3449e9c385904d16
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-06-04 11:11:14 +0530

    c1

---
+++ covert.js
@@ -0,0 +1,14 @@
+const args = process.argv.slice(2);
+
+if (args.length !== 3) {
+  console.log(`
+❌ Incorrect usage!
+
+✅ Format:
+  node convert.js <value> <from_unit> <to_unit>
+
+🔁 Supported conversions:
+  km ⇄ mi
+  kg ⇄ lb
+  C ⇄ F
+`);
