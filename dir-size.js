243377fe7f1e54de7ba3156a1465e6c8611a1bc2
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-06-28 13:25:25 +0530

    c3

--- dir-size.js
+++ dir-size.js
@@ -2,3 +2,7 @@ const fs = require("fs")
 const path = require("path")
 const args = process.argv.slice(2)
 const dirPath = args[0]
+if (!dirPath) {
+  console.log("Usage: dir-size.js <path-to-directory>")
+  process.exit(1)
+}
