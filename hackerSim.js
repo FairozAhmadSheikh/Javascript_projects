f63fd809851a8f928abef9c2372fb77ba5b65b0a
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-07-06 17:02:27 +0530

    c4

--- hackerSim.js
+++ hackerSim.js
@@ -5,10 +5,17 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-/ Utility: Typewriter Effect
+// Utility: Typewriter Effect
 function typeWriter(text, speed = 10) {
   return new Promise((resolve) => {
     let i = 0;
     const interval = setInterval(() => {
       process.stdout.write(text[i]);
       i++;
+      if (i === text.length) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, speed);
+  });
+}
