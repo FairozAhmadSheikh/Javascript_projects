59623a25d63f0fd649a67744a972842c23f97c75
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-08-17 23:34:34 +0530

    c3

--- inheritance.js
+++ inheritance.js
@@ -4,6 +4,12 @@ class Person {
         this.lastName = lastName;
         this.age = age;
     }
+    // Method to return full name
     getFullName() {
         return `${this.firstName} ${this.lastName}`;
     }
+    // Method to introduce the person
+    introduce() {
+        console.log(`Hello, my name is ${this.getFullName()} and I am ${this.age} years old.`);
+    }
+}
